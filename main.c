f71aa82dbed02b585d83f27bb27cea96b2c04805
Hendrik Quintelier <hendrik.quintelier@student.kuleuven.be>
2025-02-04 22:02:49 +0100

    initialized globals in main

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "track_files_PRIVATE//track_generation.h"
 
 int main() {
+    initialize_globals();
     initialize_grid();  // Initialize grid from track_generation
     create_loop_track();  // Create the track layout
 
